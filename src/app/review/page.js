1d21d620e66dfa253b5d33f6f7ab99acd80e67a2
Pimplay-Aakash <pimplayaakash@gmail.com>
2024-02-19 11:17:56 +0530

    made changes in  review page

--- src/app/review/page.js
+++ src/app/review/page.js
@@ -8,8 +8,8 @@ import { RiEditFill } from "react-icons/ri";
 
 const page = () => {
   return (
-    <div className='p-10 '>
-        <div className='w-full mb-5 p-5'>
+    <div className='p-10 bg-gray-400'>
+        <div className='w-full mb-5 p-5 bg-white'>
             <div className='flex justify-between w-full mb-4'>
                 <p className='font-medium text-xl'>Personal Information</p>
                 <div className='flex items-center gap-4'>
@@ -20,8 +20,8 @@ const page = () => {
             <PersonalInfo/>
         </div>
 
-        <div className='mb-5 p-5'>
-            <div className='flex justify-between w-full mb-4'>
+        <div className='mb-5 p-5 bg-white'>
+            <div className='flex justify-between w-full mb-4 '>
                 <p className='font-medium text-xl'>Equipment Details</p>
                 <div className='flex items-center gap-4'>
                     <button className='flex items-center border px-5 py-2'><RiEditFill /><p>Edit</p></button>
@@ -30,7 +30,7 @@ const page = () => {
             <ProfessionalInfo/>
         </div>
 
-        <div className='mb-5 p-5'>
+        <div className='mb-5 p-5 bg-white'>
             <div className='flex justify-between w-full mb-4'>
                 <p className='font-medium text-xl'>Document</p>
                 <div className='flex items-center gap-4'>
@@ -45,7 +45,7 @@ const page = () => {
             
         </div>
 
-        <div className='mb-5 p-5'>
+        <div className='mb-5 p-5 bg-white'>
              <div className='flex justify-between w-full mb-4'>
                 <p className='font-medium text-xl'>Professional Information</p>
                 <div className='flex items-center gap-4'>
